bda016600f11aadaaa46a69d04a79783c271852a
leansis <leansisproductivity@gmail.com>
2026-05-26 09:13:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function renderApps() {
                 </div>
                 <div style="display:flex; flex-direction:row; background:white;">
                     <img src="${screen.image1}" class="img-enlace" alt="${screen.title}" class="screenshot-img" onerror="this.src='https://placehold.co/1200x800/f8f9fa/333333?text=${encodeURIComponent(screen.title)}'">
-                    <img src="${screen.image2}" class="img-enlace" style="width:34%;" alt="${screen.title}" class="screenshot-img" onerror="this.src='https://placehold.co/1200x800/f8f9fa/333333?text=${encodeURIComponent(screen.title)}'">
+                    <img src="${screen.image2}" class="img-enlace" alt="${screen.title}" class="screenshot-img" onerror="this.src='https://placehold.co/1200x800/f8f9fa/333333?text=${encodeURIComponent(screen.title)}'">
                     <img src="${screen.image3}" class="img-enlace" alt="${screen.title}" class="screenshot-img" onerror="this.src='https://placehold.co/1200x800/f8f9fa/333333?text=${encodeURIComponent(screen.title)}'">
                 </div>
             </div>
